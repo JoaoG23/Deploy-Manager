595135e9e94d989afad19d01a4f3ac4496063c55
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2024-05-03 18:12:11 -0300

    test: Adição de teste noeo na atualização do apps

--- src/test/java/com/deploy/manager/entities/applications/services/ApplicationServicesTest.java
+++ src/test/java/com/deploy/manager/entities/applications/services/ApplicationServicesTest.java
@@ -1,7 +1,9 @@
 package com.deploy.manager.entities.applications.services;
 
 import com.deploy.manager.entities.applications.dtos.ApplicationDTO;
+import com.deploy.manager.entities.applications.model.ApplicationModel;
 import com.deploy.manager.entities.applications.repository.ApplicationRepository;
+import com.deploy.manager.infra.HandlerErros.NotFoundCustomException.NotFoundCustomException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +13,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 class ApplicationServicesTest {
@@ -47,17 +53,55 @@ class ApplicationServicesTest {
 	}
 
 	@Test
+	@DisplayName("Should find all")
+	void findAllCase1()  {
+
+		ApplicationModel appModel = new ApplicationModel();
+		appModel.setId(1L);
+		appModel.setNameApplication("App");
+		appModel.setDescription("Descrição da aplicação");
+
+		// Define o comportamento do método findAll() para retornar uma lista com o appModel
+		when(applicationRepository.findAll()).thenReturn(Arrays.asList(appModel));
+
+		var result = applicationServices.findAll();
+
+		System.out.println(result);
+		assertThat(result.isEmpty()).isFalse();
+	}
+	@Test
 	@DisplayName("Should throw Exception when application don't exists ")
 	void updateApplicationCase1() throws Exception {
 
 		ApplicationDTO app = new ApplicationDTO();
+		app.setId(2L);
 		app.setNameApplication("App");
 		app.setDescription("Descrição da aplicação");
 
 		var appFound = applicationServices.create(app);
-//		applicationServices.updateById(1L, appFound);
-
-		verify(applicationRepository, times(1)).save(any());
+		// Capturar a excessao
+		Exception thrown = Assertions.assertThrows(NotFoundCustomException.class, ()-> {
+			applicationServices.updateById(1L, appFound);
+		});
+		Assertions.assertEquals("Application not found with id: 1", thrown.getMessage());
 	}
+
+	@Test
+	@DisplayName("Should update apps ")
+	void updateApplicationCase1() throws Exception {
+
+		ApplicationDTO app = new ApplicationDTO();
+		app.setId(2L);
+		app.setNameApplication("App");
+		app.setDescription("Descrição da aplicação");
+
+		var appFound = applicationServices.create(app);
+		// Capturar a excessao
+		Exception thrown = Assertions.assertThrows(NotFoundCustomException.class, ()-> {
+			applicationServices.updateById(1L, appFound);
+		});
+		Assertions.assertEquals("Application not found with id: 1", thrown.getMessage());
+	}
+
 
 }
